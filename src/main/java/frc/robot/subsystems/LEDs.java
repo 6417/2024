45e84b63fd365387384674dbc0c949d4e523a44e
Eragon24z <dracheeragon3@gmail.com>
2024-03-21 21:12:02 +0100

    make maybemaybe working rainbow led

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -21,17 +21,10 @@ public class LEDs extends Module {
 
     private static AddressableLEDBuffer ledsBuffer;
 
-    private static int index;
-
     private Timer walkTroughTimer = new Timer(50, new ActionListener() {
-        int i = 0;
-
         @Override
         public void actionPerformed(ActionEvent e) {
-            ledsBuffer.setRGB(i - 1, 0, 0, 255);
-            ledsBuffer.setRGB(i, 255, 0, 0);
-            i %= Constants.LEDs.bufferLength;
-            setData();
+            regenbogen();
         }
     });
 
@@ -62,7 +55,6 @@ public class LEDs extends Module {
 
         leds = new AddressableLED(Constants.LEDs.pwmPort);
         leds.setLength(Constants.LEDs.bufferLength);
-        index = 0;
 
         ledsBuffer = new AddressableLEDBuffer(Constants.LEDs.bufferLength);
 
@@ -100,25 +92,54 @@ public class LEDs extends Module {
         setData();
     }
 
-    static void updateAnimation() {
-        // Löschen des vorherigen Schweifs und Erlöschen der LEDs, die nicht mehr zum
-        // Schweif gehören
-        for (int i = 0; i < 15; i++) {
-            if (i >= index && i < index + 4) {
-                int brightness = 255 + (index - i) * 50; // Dunkler werden für die 4 LEDs hinter der Schweifspitze
-                ledsBuffer.setRGB(i, brightness, 0, 0); // Rote LEDs auf dem rechten Streifen
-            } else if (i >= index + 4) {
-                ledsBuffer.setRGB(i, 255, 0, 0); // Erlöschen der LEDs, die nicht mehr zum Schweif gehören
-            }
+    private int hueL = 0;
+    private int hueR = 0;
+    private int round = 0;
 
-            if (i + 15 >= index + 15 && i + 15 < index +4+ 15) {
-                int brightness = 255 + (index - i) * 50; // Dunkler werden für die 4 LEDs hinter der Schweifspitze
-                ledsBuffer.setRGB(i + 15, brightness, 0, 0); // Rote LEDs auf dem linken Streifen
-            } else if (i + 15 >= index + 4+15) {
-                ledsBuffer.setRGB(i + 15, 0, 0, 0); // Erlöschen der LEDs, die nicht mehr zum Schweif gehören
+    private void regenbogen() {
+        for (int i = 0; i < Constants.LEDs.bufferLengthLeft; i++) {
+            hueL += ((round + 1) * 360 / 15);
+            if (hueL >= 360) {
+                hueL -= 360;
             }
+            ledsBuffer.setHSV(i, hueL, 1, 1);
         }
-        // Aktualisieren des Index für den nächsten Schweif
-        index = (index + 1) % 15;
+
+        for (int i = 15; i < Constants.LEDs.bufferLengthRight; i++) {
+            hueR += ((round + 1) * 360 / 18);
+            if (hueR >= 360) {
+                hueR -= 360;
+            }
+            ledsBuffer.setHSV(i, hueR, 1, 1);
+        }
+        round+=1;
+        setData();
     }
+
+    // static void updateAnimation() {
+    // // Löschen des vorherigen Schweifs und Erlöschen der LEDs, die nicht mehr zum
+    // // Schweif gehören
+    // for (int i = 0; i < 15; i++) {
+    // if (i >= index && i < index + 4) {
+    // int brightness = 255 + (index - i) * 50; // Dunkler werden für die 4 LEDs
+    // hinter der Schweifspitze
+    // ledsBuffer.setRGB(i, brightness, 0, 0); // Rote LEDs auf dem rechten Streifen
+    // } else if (i >= index + 4) {
+    // ledsBuffer.setRGB(i, 255, 0, 0); // Erlöschen der LEDs, die nicht mehr zum
+    // Schweif gehören
+    // }
+
+    // if (i + 15 >= index + 15 && i + 15 < index + 4 + 15) {
+    // int brightness = 255 + (index - i) * 50; // Dunkler werden für die 4 LEDs
+    // hinter der Schweifspitze
+    // ledsBuffer.setRGB(i + 15, brightness, 0, 0); // Rote LEDs auf dem linken
+    // Streifen
+    // } else if (i + 15 >= index + 4 + 15) {
+    // ledsBuffer.setRGB(i + 15, 0, 0, 0); // Erlöschen der LEDs, die nicht mehr zum
+    // Schweif gehören
+    // }
+    // }
+    // // Aktualisieren des Index für den nächsten Schweif
+    // index = (index + 1) % 15;
+    // }
 }
